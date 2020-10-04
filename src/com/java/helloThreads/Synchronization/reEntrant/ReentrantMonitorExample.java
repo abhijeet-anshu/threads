6a766edd39513c5149dc959acb8208f5160c142d
abhijeet-anshu <abhiva.adi@gmail.com>
2020-10-04 18:52:31 +0530

    re-entrant monitor

---
+++ src/com/java/helloThreads/Synchronization/reEntrant/ReentrantMonitorExample.java
@@ -0,0 +1,26 @@
+package com.java.helloThreads.Synchronization.reEntrant;
+
+public class ReentrantMonitorExample
+{
+	public static void main(String[] args)
+	{
+
+		Runnable r = () ->
+						new Object()
+						{
+							public synchronized void m()
+							{
+								n(); //m already holds monitor of this class, but now invokes for n.
+								System.out.println("i do some wrap up");
+							}
+
+							public synchronized void n()
+							{
+								System.out.println("i do some core work");
+							}
+						}.m();
+
+		new Thread(r).start();
+	}
+
+}
